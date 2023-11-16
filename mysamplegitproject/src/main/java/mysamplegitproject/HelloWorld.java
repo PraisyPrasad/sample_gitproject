4305f3d5bb4ec1c8c34dcbe50cd31db3a000b545
PraisyPrasad <praisyprasad123@gmail.com>
2023-11-17 00:12:07 +0530

    initial commit

---
+++ mysamplegitproject/src/main/java/mysamplegitproject/HelloWorld.java
@@ -0,0 +1,8 @@
+package mysamplegitproject;
+
+public class HelloWorld {
+    public static void main(String[] args) {
+        System.out.println("Hello World");
+        System.out.println("Welcome to sample project");
+    }
+}
